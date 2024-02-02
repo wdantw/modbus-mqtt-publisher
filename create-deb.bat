2a27fff84c6362f9dcaaccdeedfd7015360ccf79
dant <wdantw@gmail.com>
2024-02-02 23:15:00 +0300

    Исправление

--- create-deb.bat
+++ create-deb.bat
@@ -1,3 +1,5 @@
-dotnet publish -c Release
+del /F /S /Q publish\linux-arm
+dotnet publish Src\MudbusMqttPublisher.sln -c Release --sc false -r linux-arm -p:PublishSingleFile=true -o publish\linux-arm -p:OptimizationPreference=Speed
+dotnet tool restore
 dotnet make-deb Setup\app.debspec
 pause
